31ffdd12b01d8507382075d0bd22a08e3c4043f4
Kaai Suzuki <suzydp@me.com>
2018-06-03 15:20:13 -0700

    Make details of App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import twitterImage from './assets/icons/twitter.svg';
+import facebookImage from './assets/icons/facebook.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Header">
+          <div className="Header-logo">Logo</div>
+          <ul className="Header-nav">
+            <li>About Me</li>
+            <li>About Me</li>
+            <li>Contact</li>
+          </ul>
+          <div className="Content">
+            This is the Content
+          </div>
+          <div className="Footer">
+            <p>Copyright © 2015 Designova</p>
+            <div className="Footer-social">
+              <img src={twitterImage} />
+              <img src={facebookImage} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
