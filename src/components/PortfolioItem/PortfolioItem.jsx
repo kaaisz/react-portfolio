9d823e1800609e1bd1c182c61de57eb5ce46df43
Kaai Suzuki <suzydp@me.com>
2018-06-03 23:23:23 -0700

    Add Hover State

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 export default class PortfolioItem extends Component{
+  state = {
+    isHovered: false,
+  }
+
+  handleHoverIn = () => {
+    this.setState({
+      isHovered:true
+    });
+  }
+
+  handleHoversOut = () => {
+    this.setState({
+      isHovered:false
+    });
+  }
   render(){
+    const { isHovered } = this.state;
     return(
-      <div className="PortfolioItem">
+      <div className="PortfolioItem" onMouseOver={this.handleHoverIn}onMouseLeave={this.handleHoverOut}>
         <div className="PortfolioItem-image">
           <img src={GlobalClock} alt="Global Clock" />
-          <button className="PortfolioItem-button">See Project</button>
+          {
+            isHoverd &&(<button className="PortfolioItem-button">See Project</button>)
+          }
         </div>
         <div className="PortfolioItem-details">
           <h3>Project Name</h3>
