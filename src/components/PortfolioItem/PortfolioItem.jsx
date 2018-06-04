4106a26945d82272aca9ca123fd5f3dc3d005893
Kaai Suzuki <suzydp@me.com>
2018-06-03 23:16:19 -0700

    Make details of "PortfolioItem" and add other .scss files

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -3,7 +3,16 @@ import React, { Component } from 'react'
 export default class PortfolioItem extends Component{
   render(){
     return(
-      <div className="PortfolioItem">Hi</div>
+      <div className="PortfolioItem">
+        <div className="PortfolioItem-image">
+          <img src={GlobalClock} alt="Global Clock" />
+          <button className="PortfolioItem-button">See Project</button>
+        </div>
+        <div className="PortfolioItem-details">
+          <h3>Project Name</h3>
+          <p>Project Categories</p>
+        </div>
+      </div>
     )
   }
 }
